2a2331b99096bf0f257e96d84b79c6d38b02de5b
matteopriarolo <matteo.priarolo@istitutoeinstein.eu>
2026-04-14 11:47:44 +0200

    awd

--- login-logout/Informazione_azienda_farmaceutica/db.sql
+++ login-logout/Informazione_azienda_farmaceutica/db.sql
@@ -1,48 +1,46 @@
-CREATE azienda_farmaceutica IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS azienda_farmaceutica;
 USE azienda_farmaceutica;
 
-create TABLE personale (
-    CF CHARACTER(16) PRIMARY KEY,
-    Matricola varchar(60),
-    Nome varchar(20),
-    Cognome varchar(20),
-    DataNascita date,
-    Ruolo varchar(20),
-    Ambito varchar(20),
-    Psw text
+CREATE TABLE IF NOT EXISTS personale (
+    CF CHAR(16) PRIMARY KEY,
+    Matricola VARCHAR(60),
+    Nome VARCHAR(20),
+    Cognome VARCHAR(20),
+    DataNascita DATE,
+    Ruolo VARCHAR(20),
+    Ambito VARCHAR(20),
+    Psw TEXT
 );
 
-CREATE TABLE visite (
-    ID int PRIMARY KEY AUTO_INCREMENT,
-    ID_paziente int,
-    Data_visita datetime,
-    Luogo varchar(40),
-    Svolta datetime,
-    Fine datetime,
-    
-    CF_personale int,
+CREATE TABLE IF NOT EXISTS professionisti (
+    CF CHAR(16) PRIMARY KEY,
+    Matricola VARCHAR(60),
+    Nome VARCHAR(20),
+    Cognome VARCHAR(20),
+    DataNascita DATE,
+    Ruolo VARCHAR(20),
+    Ambito VARCHAR(20),
+    Psw TEXT
+);
+
+CREATE TABLE IF NOT EXISTS informatori (
+    CF CHAR(16) PRIMARY KEY,
+    Matricola VARCHAR(60),
+    Nome VARCHAR(20),
+    Cognome VARCHAR(20),
+    DataNascita DATE,
+    Ruolo VARCHAR(20),
+    Ambito VARCHAR(20),
+    Psw TEXT
+);
+
+CREATE TABLE IF NOT EXISTS visite (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ID_paziente INT,
+    Data_visita DATETIME,
+    Luogo VARCHAR(40),
+    Svolta DATETIME,
+    Fine DATETIME,
+    CF_personale CHAR(16),
     FOREIGN KEY (CF_personale) REFERENCES personale(CF)
 );
-
-CREATE TABLE professionisti (
-    CF CHARACTER(16) PRIMARY KEY,
-    Matricola varchar(60),
-    Nome varchar(20),
-    Cognome varchar(20),
-    DataNascita date,
-    Ruolo varchar(20),
-    Ambito varchar(20),
-    Psw text
-);
-
-CREATE TABLE informatori (
-    CF CHARACTER(16) PRIMARY KEY,
-    Matricola varchar(60),
-    Nome varchar(20),
-    Cognome varchar(20),
-    DataNascita date,
-    Ruolo varchar(20),
-    Ambito varchar(20),
-    Psw text
-)
-    
